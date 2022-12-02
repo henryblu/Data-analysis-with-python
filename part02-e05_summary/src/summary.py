8540629d19eb4a61f70aaaa6f956ab3793db05b9
henry <henry.s.blue@gmail.com>
2022-12-02 20:19:19 +0200

    week 2 exercise 5 solution

--- part02-e05_summary/src/summary.py
+++ part02-e05_summary/src/summary.py
@@ -7,14 +7,13 @@ import sys
 def std(numbers):
     """Calculate the standard deviation of a list of numbers."""
     average = sum(numbers)/len(numbers)
-    return (sum([(x-average)**2 for x in numbers])/len(numbers))**0.5
+    sum_of_squares = 0
+    for number in numbers:
+        sum_of_squares += (number - average)**2
+    return (sum_of_squares/(len(numbers)-1))**0.5
 
 def summary(filename):
-    '''
-    # summary gets a filename as a parameter. 
-    # The input file should contain a floating point number on each line of the file. 
-    # summary reads these numbers and then return a triple containing the sum, average, and standard deviation of these numbers for the file.
-    '''
+    """Read the file and return a triple containing the sum, average, and standard deviation of the numbers in the file."""
 
     with open(filename) as f:
         numbers = []
@@ -22,14 +21,15 @@ def summary(filename):
             try :
                 numbers.append(float(line))
             except ValueError:
-                pass
-        ret = f"File: {filename} Sum: {round(sum(numbers), 6)} Average: {round(sum(numbers)/len(numbers),6)} Standard Deviation: {round(std(numbers),6)}"
+                print(f"Invalid value in file {filename}: {line}")
+        ret = sum(numbers), sum(numbers)/len(numbers), std(numbers)
         return ret
 
 def main():
     """The main function calls the function summary for each filename in the list sys.argv[1:] of command line parameters. """
     for filename in sys.argv[1:]:
-        print(summary(filename))
+        x = summary(filename)
+        print(f"File: {filename} Sum: {format(x[0], '.6f')} Average: {format(x[1], '.6f')} Stddev: {format(x[2], '.6f')}")
 
 if __name__ == "__main__":
     main()

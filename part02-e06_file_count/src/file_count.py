8851307f4f0a38d039a8b4be1e25580071f4ee70
henry <henry.s.blue@gmail.com>
2022-12-02 20:44:33 +0200

    week 2 exercises 6 and 7

--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -3,10 +3,22 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    """Count the number of lines, words, and characters in a file. Return a tuple containing the counts."""
+    with open(filename) as f:
+        lines = 0
+        words = 0
+        characters = 0
+        for line in f:
+            lines += 1
+            words += len(line.split())
+            characters += len(line)
+        return lines, words, characters
 
 def main():
-    pass
+    """The main function calls the function file_count for each filename in the list sys.argv[1:] of command line parameters. """
+    for filename in sys.argv[1:]:
+        x = file_count(filename)
+        print(f'{x[0]}\t{x[1]}\t{x[2]}\t{filename}')
 
 if __name__ == "__main__":
     main()
--- part02-e07_file_extensions/src/file_extensions.py
+++ part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
-
+import sys
 def file_extensions(filename):
-    return ([], {})
+    '''Returns a list of filenames with no extensions and a dictionary of file extensions and file names in the file filename.'''
+    with open(filename) as f:
+        files = []
+        extensions = {}
+        for line in f:
+            line = line.strip()
+            if '.' in line:
+                extension = line.split('.')[-1]
+                if extension in extensions:
+                    extensions[extension].append(line)
+                else:
+                    extensions[extension] = [line]
+            else:
+                files.append(line)
+        return files, extensions
+
 
 def main():
-    pass
+    '''Main function'''
+    files, extensions = file_extensions("filenames.txt")
+    print(f"{len(files)} files with no extension")
+    for extension in extensions:
+        print(f"{extension} {len(extensions[extension])}")    
 
 if __name__ == "__main__":
     main()

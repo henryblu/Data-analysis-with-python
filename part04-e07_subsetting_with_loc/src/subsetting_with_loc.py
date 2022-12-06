1b4924afbdf7a5618d67f20a1be14bdd8e8518f3
henry <henry.s.blue@gmail.com>
2022-12-06 14:55:07 +0200

    week 4 exercises 7 and 8 solutions

--- part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
+++ part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
@@ -3,9 +3,13 @@
 import pandas as pd
 
 def subsetting_with_loc():
-    return None
+    '''this function reads the municipal data and returns the population and percent of swedish speakers and foreign citizens'''
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df = pd.DataFrame(df.loc['Akaa':'Äänekoski', ['Population', 'Share of Swedish-speakers of the population, %', "Share of foreign citizens of the population, %"]])
+    return df
 
 def main():
+    subsetting_with_loc()
     return
 
 if __name__ == "__main__":
--- part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -3,9 +3,14 @@
 import pandas as pd
 
 def subsetting_by_positions():
-    return pd.DataFrame()
+    ''' reads the UK-top40 and Returns the top 10 entries and only the columns Title and Artist'''
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df = pd.DataFrame(df.iloc[0:10, [2, 3]])
+
+    return df
 
 def main():
+    subsetting_by_positions()
     return
 
 if __name__ == "__main__":

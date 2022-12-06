df1d70ae7846b020d01dba89b72211cb786c6f88
henry <henry.s.blue@gmail.com>
2022-12-06 14:55:37 +0200

    week 4 exercises 11 solution

--- part04-e11_below_zero/src/below_zero.py
+++ part04-e11_below_zero/src/below_zero.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def below_zero():
-    return 0
+    '''this funciton reads the weather data and returns  the number of days when the temperature was below zero'''
+    
+    df = pd.read_csv('src/kumpula-weather-2017.csv')
+    df = df[df['Air temperature (degC)'] < 0]
+    des = df.describe()
+    return des.loc['count', 'Year']
 
 def main():
-    return
+    print(f"Number of days below zero: {below_zero():.0f}")
     
 if __name__ == "__main__":
     main()

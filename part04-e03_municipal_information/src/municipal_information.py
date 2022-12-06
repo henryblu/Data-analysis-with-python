7c86689000e44ee261fa364181dbac80d02d456e
henry <henry.s.blue@gmail.com>
2022-12-06 14:52:29 +0200

    week 4 exercises 3 and 4

--- part04-e03_municipal_information/src/municipal_information.py
+++ part04-e03_municipal_information/src/municipal_information.py
@@ -3,7 +3,14 @@
 import pandas as pd
 
 def main():
-    return
+    '''reads the file municipal.tsv and prints the shape of the DataFrame and the column names in the following format "col1, col2, col3, ...".'''
+    x = pd.read_csv("src/municipal.tsv", sep="\t")
+    #x = pd.read_csv("municipal.tsv", sep="\t")
+    print(f"Shape: {x.shape[0]},{x.shape[1]}")
+    print("Columns:")
+    for i in range(len(x.columns)):
+        print(x.columns[i])
+     
 
 
 if __name__ == "__main__":
--- part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
+++ part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
@@ -3,9 +3,13 @@
 import pandas as pd
 
 def municipalities_of_finland():
-    return None
+    '''Takes in a municipal dataframe. Returns a DataFrame containing only rows about municipalities.'''
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col="Region 2018")
+    return df['Akaa':'Äänekoski']
     
 def main():
+    #df = pd.read_csv("src/municipal.tsv", sep="\t", index_col="Region 2018")
+    print(municipalities_of_finland())
     return
     
 if __name__ == "__main__":

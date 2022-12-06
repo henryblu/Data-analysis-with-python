d26803b58a6dc49e02f345b6010757a9dd5b9958
henry <henry.s.blue@gmail.com>
2022-12-06 14:51:42 +0200

    week 4 exercises 1 and 2

--- part04-e02_powers_of_series/src/powers_of_series.py
+++ part04-e02_powers_of_series/src/powers_of_series.py
@@ -3,10 +3,16 @@
 import pandas as pd
 
 def powers_of_series(s, k):
-    return None
+    '''takes in a series s, and a integer k, and returns a dataframe which has the powers of the series s from 1 to k as columns.'''
+    df = pd.DataFrame()
+    for i in range(1, k+1):
+        df[i] = s ** i
+    return df
     
 def main():
-    return
+    s = pd.Series([1,2,3,4], index=list("abcd"))
+    print(s)  
+    print(powers_of_series(s, 3))  
     
 if __name__ == "__main__":
     main()

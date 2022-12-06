d85f516f5dfa4b6765db3477a3f03d1fda038539
henry <henry.s.blue@gmail.com>
2022-12-06 14:55:25 +0200

    week 4 exercises 9 and 10 solutions

--- part04-e09_snow_depth/src/snow_depth.py
+++ part04-e09_snow_depth/src/snow_depth.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def snow_depth():
-    return 0.0
+    '''reads the kumpula-weather-2017.csv file and returns the maximum snow depth in the year 2017'''
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    df =  df.describe()
+    return df.loc['max', 'Snow depth (cm)']
+
 
 def main():
-    return
+    x = snow_depth()
+    print(f"Max snow depth: {x:.1f}")
 
 if __name__ == "__main__":
     main()
--- part04-e10_average_temperature/src/average_temperature.py
+++ part04-e10_average_temperature/src/average_temperature.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def average_temperature():
-    return 0.0
+    '''function average_temperature reads the weather data set and returns the average temperature in July.'''
+    df = pd.read_csv('src/kumpula-weather-2017.csv')
+    df = df.loc[df['m'] == 7]
+    sum = df.describe()
+    return sum.loc['mean', 'Air temperature (degC)']
 
 def main():
-    return
+    x = average_temperature()
+    print(f"Average temperature in July: {x:.1f}")
 
 if __name__ == "__main__":
     main()

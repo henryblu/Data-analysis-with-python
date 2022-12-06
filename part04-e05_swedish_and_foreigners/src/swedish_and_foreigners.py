8de438b093dd1fc791a2a2dace929e2de9d4c39c
henry <henry.s.blue@gmail.com>
2022-12-06 14:54:56 +0200

    week 4 exercises 5 and 6 solutions

--- part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -3,9 +3,16 @@
 import pandas as pd
 
 def swedish_and_foreigners():
-    return None
+    '''this function reads the municipal data and returns the municipalities that have more than 5% swedish speakers and foreign citizens'''
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df = df['Akaa':'Äänekoski']
+    df = df[df['Share of Swedish-speakers of the population, %'] > 5.0]
+    df = df[df['Share of foreign citizens of the population, %'] > 5.0]
+    return df[['Population','Share of Swedish-speakers of the population, %','Share of foreign citizens of the population, %']]
+
 
 def main():
+    print(swedish_and_foreigners())
     return
 
 if __name__ == "__main__":
--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -3,9 +3,16 @@
 import pandas as pd
 
 def growing_municipalities(df):
-    0.0
+    '''Returns the percentage of municipalities with growing population'''
+    df1 = df[df['Population change from the previous year, %'] > 0]
+    return len(df1) / len(df)
+    
 
 def main():
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df = df['Akaa':'Äänekoski']
+    growing = growing_municipalities(df)
+    print(f"Proportion of growing municipalities: {100*growing:.1f}%")
     return
 
 if __name__ == "__main__":

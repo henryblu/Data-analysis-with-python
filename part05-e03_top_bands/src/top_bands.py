8f3cebba0b71664fd1528b1d234918e6815d5a0a
henry <henry.s.blue@gmail.com>
2022-12-11 21:27:15 +0200

    p5 ex3

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -3,10 +3,18 @@
 import pandas as pd
 
 def top_bands():
-    return pd.DataFrame()
-
+    '''Reads the data from the file bands.tsv and UK-top40-1964-1-2.tsv and returns a merged dataframe'''
+    df1 = pd.read_csv('src/bands.tsv', sep='\t')
+    df2 = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')
+    df1["Band"] = df1["Band"].str.upper()
+    df = pd.merge(df1, df2, left_on='Band', right_on='Artist')
+    return df
+    
 def main():
-    return
+    df = top_bands()
+    print("Shape:", df.shape)
+    print("Column names:\n", df.columns)
+    print(df.head())
 
 if __name__ == "__main__":
     main()

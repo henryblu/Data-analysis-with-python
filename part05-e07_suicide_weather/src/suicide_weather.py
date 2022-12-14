4e964e0f3fb3285a8543edec13e425f4ee23ca2e
henry <henry.s.blue@gmail.com>
2022-12-14 14:23:34 +0200

    part 5 ex7

--- part05-e07_suicide_weather/src/suicide_weather.py
+++ part05-e07_suicide_weather/src/suicide_weather.py
@@ -2,8 +2,18 @@
 
 import pandas as pd
 
-    
+def suicide_fractions():
+    '''this function loads the dataset suicide_fractions from the src folder. It returns a Series that has the country as the row index and the mean fraction of suicides per population as the coulmns'''
+
+    df = pd.read_csv('src/who_suicide_statistics.csv', index_col='country')
+    df['fraction']=df['suicides_no']/df['population']
+    means = df.groupby('country')[['fraction']].mean()
+
+    return pd.Series(means['fraction'])
+
 def suicide_weather():
+    '''print hello world'''
+    print()
     return (0, 0, 0, 0.0)
 
 def main():

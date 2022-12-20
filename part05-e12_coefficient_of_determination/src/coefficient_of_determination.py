d24b4af04c88d166bd450bfb491af2e1e9cdf08f
henry <henry.s.blue@gmail.com>
2022-12-19 21:12:05 -0800

    part 5 exercise 12

--- part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -5,9 +5,24 @@ from sklearn import linear_model
 
 
 def coefficient_of_determination():
-    return []
+    '''reads the mystery_data.tsv file and returns a list of R2 scores for all x variables and the full model'''
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    x = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    model = linear_model.LinearRegression()
+    model.fit(x, y)
+
+    ret = [model.score(x, y)]
+    for i in x:
+        model.fit(x[[i]], y)
+        ret.append(model.score(x[[i]], y))
+    return ret
     
 def main():
+    x = coefficient_of_determination()
+    print(f"R2-score with feature(s) X: {x[0]}")
+    for i in range(1,len(x)):
+        print(f"R2-score with feature(s) X{i}: {x[i]}")
 
 if __name__ == "__main__":
     main()

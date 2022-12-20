ede93afccf38bff9f1b55ab0e2c1dc6b3c943200
henry <henry.s.blue@gmail.com>
2022-12-20 15:01:22 -0800

    part6-e05 solution

--- part06-e05_plant_clustering/src/plant_clustering.py
+++ part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import scipy
+from sklearn.datasets import load_iris
+from sklearn import cluster
+from sklearn import metrics
 
 
 def find_permutation(n_clusters, real_labels, labels):
@@ -13,7 +16,18 @@ def find_permutation(n_clusters, real_labels, labels):
     return permutation
 
 def plant_clustering():
-    return 0.0
+    '''this function returns the acuracy of the clustering model on the iris dataset'''
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+
+    model = cluster.KMeans(n_clusters=3, random_state=0)
+    model.fit(X)
+    labels = model.labels_
+    permutation = find_permutation(3, y, labels)
+    labels = [permutation[label] for label in labels]
+    return metrics.accuracy_score(y, labels)
+
 
 def main():
     print(plant_clustering())

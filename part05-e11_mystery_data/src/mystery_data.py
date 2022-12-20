f166c549b9b6d47f223fb12a140a5a0f0bfe1b67
henry <henry.s.blue@gmail.com>
2022-12-19 21:11:54 -0800

    part 5 exercise 11

--- part05-e11_mystery_data/src/mystery_data.py
+++ part05-e11_mystery_data/src/mystery_data.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 def mystery_data():
-    return []
+    ''' Read the tab separated file mystery_data.tsv. Its first five columns define the features, and the last column is the response..csv and returns the coefficients of the fitted line.'''
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    x = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    model = LinearRegression(fit_intercept=False)
+    model.fit(x, y)
+    return model.coef_
+
 
 def main():
     coefficients = mystery_data()
+    for i in range(len(coefficients)):
+        print(f'Coefficient of X{i+1} is {coefficients[i]}')
     # print the coefficients here
     
 if __name__ == "__main__":

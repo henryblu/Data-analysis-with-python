d80db8c4e329833ddbcc878285a8eb14c7396874
henry <henry.s.blue@gmail.com>
2022-12-20 15:00:47 -0800

    part6-e02 solution

--- part06-e02_plant_classification/src/plant_classification.py
+++ part06-e02_plant_classification/src/plant_classification.py
@@ -5,8 +5,16 @@ from sklearn.model_selection import train_test_split
 from sklearn import naive_bayes
 from sklearn import metrics
 
+
 def plant_classification():
-    return 0.0
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    model = naive_bayes.GaussianNB()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return metrics.accuracy_score(y_test, y_pred)
 
 def main():
     print(f"Accuracy is {plant_classification()}")

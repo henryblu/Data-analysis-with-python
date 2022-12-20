c98beab05f5897002eb1c24aec2e95a500a50938
henry <henry.s.blue@gmail.com>
2022-12-19 18:04:46 -0800

    week 5 exercise 7

--- part05-e07_suicide_weather/src/suicide_weather.py
+++ part05-e07_suicide_weather/src/suicide_weather.py
@@ -12,12 +12,27 @@ def suicide_fractions():
     return pd.Series(means['fraction'])
 
 def suicide_weather():
-    '''print hello world'''
-    print()
-    return (0, 0, 0, 0.0)
+    '''reads the List_of_countries_by_average_yearly_temperature file from the src folder and meerges it with the dataframe from suicie fractions. Finally it will find the spearman correlation value betweenm the suicide fraction and the temperature on a day '''
+    df1 = suicide_fractions()
+    df = pd.read_html("src/List_of_countries_by_average_yearly_temperature.html", index_col="Country")[0]
+    df.rename(columns={"Average yearly temperature (1961–1990, degrees Celsius)":"TempAvg"}, inplace=True)
+    df = pd.to_numeric(df.iloc[:, 0].str.replace("\u2212", "-"))
+
+    common_rows = pd.concat([df1, df], axis=1, join="inner")
+    Spearman_corr = common_rows["fraction"].corr(common_rows["TempAvg"], method="spearman")
+    return (len(df1), len(df), len(common_rows), Spearman_corr)
 
 def main():
-    return
+    '''prints out the results from the suicide weather function'''
+    ans = suicide_weather()
+    print(f'Suicide DataFrame has {ans[0]} rows')
+    print(f'Temperature DataFrame has {ans[1]} rows')
+    print(f'Common DataFrame has {ans[2]} rows')
+    print(f'Spearman correlation: {ans[3]} rows')
+    
 
 if __name__ == "__main__":
     main()
+
+
+

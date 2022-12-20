8a552cc13a6a4f498d5d964de254199085975865
henry <henry.s.blue@gmail.com>
2022-12-20 15:00:54 -0800

    part6-e03 solution

--- part06-e03_word_classification/src/word_classification.py
+++ part06-e03_word_classification/src/word_classification.py
@@ -37,17 +37,46 @@ def load_english():
     return lines
 
 def get_features(a):
-    return np.array([[]])
+    '''gets a one dimensional np.array, containing words, as parameter. returns a two dimensional np.array, number of times the corresponding character appears in the word.
+    '''
+    features = np.zeros((len(a), len(alphabet)))
+    for i, word in enumerate(a):
+        for j, char in enumerate(alphabet):
+            features[i, j] += word.count(char)
+    return features
 
 def contains_valid_chars(s):
+    '''gets a string as parameter. returns True if the string contains only characters in alphabet, False otherwise'''
+    for char in s:
+        if char not in alphabet_set:
+            return False
     return True
 
 def get_features_and_labels():
-    return np.array([[]]), np.array([])
+    '''returns a tuple of two np.arrays. the first array contains the features, the second array contains the labels. the labels are 0 for Finnish words and 1 for English words. '''
+    finnish = load_finnish()
+    english = load_english()
+
+    finnish = list(map(lambda s: s.lower(), finnish))
+    english = list(filter(lambda s: s[0].islower(), english))
+    english = list(map(lambda s: s.lower(), english))
+
+    finnish = list(filter(contains_valid_chars, finnish))
+    english = list(filter(contains_valid_chars, english))
+
+    features = get_features(np.array(finnish + english))
+    labels = np.array([0] * len(finnish) + [1] * len(english))
+    return features, labels
 
 
 def word_classification():
-    return []
+    '''returns a list of accuracy scores for the model'''
+    features, labels = get_features_and_labels()
+    model = MultinomialNB()
+    gen = model_selection.KFold(n_splits=5, shuffle=True, random_state=0)
+    scores = cross_val_score(model, features, labels, cv=gen)
+    return scores
+
 
 
 def main():

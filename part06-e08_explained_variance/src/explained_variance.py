20e9c25933ec4e1d4328d569b2f2fead1352762f
henry <henry.s.blue@gmail.com>
2022-12-20 15:02:00 -0800

    part6-e08 solution

--- part06-e08_explained_variance/src/explained_variance.py
+++ part06-e08_explained_variance/src/explained_variance.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
-
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from sklearn.decomposition import PCA
+import pandas as pd
 
 def explained_variance():
-    return [], []
+    '''The function reads the data from the file src/data.tsv and returns the variances of the columns and the explained variances after PCA.'''
+    df = pd.read_csv("src/data.tsv", sep='\t')
+    var = df.var(axis=0)
+    X = df.to_numpy()
+    pca = PCA(n_components=10)
+    pca.fit(X)
+    return var, pca.explained_variance_
+
 
 def main():
+    
     v, ev = explained_variance()
-    print(sum(v), sum(ev))
+    v_list = " ".join([f"{x:.3f}" for x in v])
+    ev_list = " ".join([f"{x:.3f}" for x in ev])
+
+    print(f"The variances are: {v_list}")
+    print(f"The explained variances after PCA are: {ev_list}")
+
+    sum = np.cumsum(ev)
+    terms = len(sum)
+
+    plt.plot(np.arange(1, terms + 1), sum)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
